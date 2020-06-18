6fcb907866a272501376dae75b8b6624e69ef52e
rachelli <brli@dmacc.edu>
2020-06-18 13:13:12 -0500

    add docstrings

--- fun_with_collections/basic_list.py
+++ fun_with_collections/basic_list.py
@@ -1,4 +1,18 @@
+"""
+Program name: basic_list.py
+Author: Rachel Li
+Last date modified: 06/18/2020
+
+The purpose of this program is to get input
+and print user input as list
+"""
 def get_input():
+    """
+    use reST style.
+
+    :param number: this represents user input for number
+    :return: user input with number
+    """
     #ask user input of number
     number = input("Please enter a number: ")
     #return a string
@@ -6,6 +20,13 @@ def get_input():
 
 
 def make_list():
+    """
+    use reST style.
+
+    :param x: this calls function get_input as integer
+    :return: list with number
+    :raises keyError: raises an exception
+    """
     #declare a list variable
     list = []
     # try and cast to numeric type
