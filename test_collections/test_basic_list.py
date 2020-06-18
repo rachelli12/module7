14250bac09051c6de239b87378717091a5a3fd37
rachelli <brli@dmacc.edu>
2020-06-18 13:06:44 -0500

    test passed

--- test_collections/test_basic_list.py
+++ test_collections/test_basic_list.py
@@ -3,7 +3,7 @@ from unittest.mock import patch
 from fun_with_collections import basic_list as topic1
 
 class TestList(unittest.TestCase):
-    @patch('fun_with_collections.topic1.get_input', return_value='5')
+    @patch('fun_with_collections.basic_list.get_input', return_value='5')
     def test_make_list(self, input):
         self.assertEqual(topic1.make_list(), [5,5,5])
 
