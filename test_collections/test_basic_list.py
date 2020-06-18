b4f3cd146ba9e7577aee1a44759fd3f28c151ecc
rachelli <brli@dmacc.edu>
2020-06-18 13:12:56 -0500

    add docstrings

--- test_collections/test_basic_list.py
+++ test_collections/test_basic_list.py
@@ -1,3 +1,10 @@
+"""
+Program name: test_basic_list.py
+Author: Rachel Li
+Last date modified: 06/18/2020
+
+The purpose of this program is to test basic_list.py
+"""
 import unittest
 from unittest.mock import patch
 from fun_with_collections import basic_list as topic1
