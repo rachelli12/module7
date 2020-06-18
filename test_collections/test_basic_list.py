f30b20b21d51c6a2b5e2288e304c085b97a6e4a1
rachelli <brli@dmacc.edu>
2020-06-18 12:44:38 -0500

    initial commit

---
+++ test_collections/test_basic_list.py
@@ -0,0 +1,11 @@
+import unittest
+from unittest.mock import patch
+from fun_with_collections import basic_list as topic1
+
+class TestList(unittest.TestCase):
+    @patch('fun_with_collections.topic1.get_input', return_value='5')
+    def test_make_list(self, input):
+        self.assertEqual(topic1.make_list(), [5,5,5])
+
+if __name__=='__main__':
+    unittest.main()
