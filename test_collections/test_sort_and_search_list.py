2d74a81a5e712d01564d7c491c0d9797872f6562
rachelli <brli@dmacc.edu>
2020-06-19 20:23:10 -0500

    initial commit. no test passed

---
+++ test_collections/test_sort_and_search_list.py
@@ -0,0 +1,11 @@
+import unittest
+import unittest.mock as mock
+from fun_with_collections import sort_and_search_list as ssl
+
+class TestList(unittest.TestCase):
+    def test_search_list_found(self):
+        self.assertEqual(ssl.search_list('peach'), 4)
+    def test_search_list_not_found(self):
+        with self.assertRaises(ValueError):
+            ssl.search_list('chocolate')
+
