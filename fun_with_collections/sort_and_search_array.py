ec77e0b3f4f133a74b3cbfe068878bdf843c844f
rachelli <brli@dmacc.edu>
2020-06-21 11:59:49 -0500

    initial upload

---
+++ fun_with_collections/sort_and_search_array.py
@@ -0,0 +1,30 @@
+import array as arr
+
+number_list = [1.2, 4.6, 7.8, 4.3, 5.9, 4.8, 9.1]
+n = arr.array('d',[1.2, 4.6, 7.8, 4.3, 5.9, 4.8, 9.1])
+
+def search_array(number_item):
+    try:
+    # array.index(x)
+        y = n.index(number_item)
+    #return the index of the object in the list
+        return y
+    #or -1 if the item is not found
+    except ValueError as err:
+        print('-1', err)
+        raise ValueError
+    pass
+
+def sort_array():
+    #sort the list
+    #list.sort(x)
+    number_list.sort()
+    #no return statement
+
+
+if __name__ == '__main__':
+    search_number = search_array(4.6)
+    print(search_number)
+
+    sort_array()
+    print(number_list)
