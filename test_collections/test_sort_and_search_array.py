25f00314895edba663ee35cd8f2d3859dfa6f947
rachelli <brli@dmacc.edu>
2020-06-21 12:01:41 -0500

    search_array tests passed

--- test_collections/test_sort_and_search_array.py
+++ test_collections/test_sort_and_search_array.py
@@ -4,10 +4,11 @@ from fun_with_collections import sort_and_search_array as ssa
 
 class TestArray(unittest.TestCase):
     def test_search_array_found(self):
-        self.assertEqual(ssa.search_array(), )
+        self.assertEqual(ssa.search_array(5.9), 4)
     def test_search_array_not_found(self):
         with self.assertRaises(ValueError):
-            ssa.search_array()
+            ssa.search_array(69)
+
 
 if __name__ == '__main__':
     unittest.main()
